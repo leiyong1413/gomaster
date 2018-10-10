d9ea695617f7c661493ff73f8960c724209a257e
leiyong <leiyong@gwecom.com>
2018-10-10 09:55:19 +0800

    getting started

--- gomaster.go
+++ gomaster.go
@@ -1,7 +1,35 @@
 package main
 
 import "fmt"
+import "github.com/leiyong1413/gomaster/basic/gettingstarted"
+
+type UseCaseType int
+
+// 用列枚举
+const (
+	BasicGettingStarted UseCaseType = iota
+	BasicPackage
+)
+
+// 实现一个简单工厂
+type UseCase interface {
+	Reference()
+	RunUseCase()
+}
+
+// 简单工厂
+func NewUseCase(useCaseType UseCaseType) UseCase {
+	switch useCaseType {
+	case BasicGettingStarted:
+		return &gettingstarted.GettingStarted{}
+	case BasicPackage:
+	default:
+	}
+	return nil
+}
 
 func main() {
 	fmt.Println("hello golang!!!")
+	useCase := NewUseCase(BasicGettingStarted)
+	useCase.RunUseCase()
 }
