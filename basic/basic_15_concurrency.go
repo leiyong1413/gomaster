c1df682727ecf717c9bb4387ba0aed3da08a4eca
leiyong <leiyong@gwecom.com>
2018-11-02 18:38:39 +0800

    concurrency select

--- basic/basic_15_concurrency.go
+++ basic/basic_15_concurrency.go
@@ -2,8 +2,11 @@ package basic
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
+
+// 注意：死锁，并发中只要有语句一直阻塞，就会导致死锁
 
 // Go 是并发式语言，而不是并行式语言
 
@@ -46,6 +49,47 @@ import (
 
 
 // 缓存信道
+// 无缓冲信道的发送和接收过程是阻塞的
+// 有缓冲信道，只在缓冲已满的情况，才会阻塞向缓冲信道（Buffered Channel）发送数据。同样，只有在缓冲为空的时候，才会阻塞从缓冲信道接收数据
+// 如果创建：ch := make(chan type, capacity), capacity 大于0则创建有缓冲的信道，无缓冲信道的容量默认为 0
+// 容量，指信道可以存储的值的数量， 缓冲信道的长度，指信道中当前排队的元素的个数
+
+
+// WaitGroup
+// 作用：等待一批 Go 协程全部执行结束，会一直阻塞直到这些协程全部执行完毕。
+// 工作原理：WaitGroup 使用计数器，当 Add(int count) 会传递一个 int 时，
+// WaitGroup 的计数器会 [加上] Add 的传参。要减少计数器，可以调用 WaitGroup 的 Done() 方法。
+// Wait() 方法会 [阻塞调用它] 的 Go 协程，直到计数器变为 0 后才会停止阻塞
+
+
+// 什么是select?
+// 作用：在多个发送、接受信道操作中进行选择。select 语句会一直阻塞，直到发送/接收操作准备就绪。如果有多个信道操作准备完毕，select 会随机地选取其中之一执行。
+
+func selectServer1(ch chan string)  {
+	time.Sleep(time.Second * 6)
+	ch <- "from selectServer1"
+}
+
+
+func selectServer2(ch chan string)  {
+	time.Sleep(time.Second * 3)
+	ch <- "from selectServer2"
+}
+
+func selectRandomeServer1(ch chan string)  {
+	ch <- "from selectRandomeServer1"
+}
+
+func selectRandomeServer2(ch chan string)  {
+	ch <- "from selectRandomeServer2"
+}
+
+
+func waitGroupProcess(i int, wg *sync.WaitGroup)  {
+	time.Sleep(2 * time.Second)
+	wg.Done()
+}
+
 
 func hello(done chan bool)  {
 	fmt.Println("hello go routine is going to sleep")
@@ -136,6 +180,107 @@ func UseCase15Concurrency()  {
 	go producer1(ch1)
 	for v := range ch1 {
 		fmt.Println("Received", v)
+	}
+
+	// 缓冲信道的长度和容量
+	// 容量：是指信道中可以存储的元素的个数
+	// 长度：是指信道中当前排队的元素个数
+	ch3 := make(chan string, 3)
+	ch3 <- "naveen"
+	ch3 <- "paul"
+	fmt.Println("capacity is", cap(ch3))	// 3
+	fmt.Println("length is", len(ch3))		// 2
+	fmt.Println("read value", <- ch3)		// naveen
+	fmt.Println("new length is", len(ch3))	// 1
+	fmt.Println("read value", <- ch3)			// paul
+	fmt.Println("new length is", len(ch3))	// 0
+
+	// WaitGroup
+	// wg 其初始值为零值
+	var wg sync.WaitGroup
+	for i := 0;i < 3 ; i++ {
+		wg.Add(1)
+		// 传递 wg 的地址是很重要的。如果没有传递 wg 的地址，那么每个 Go 协程将会得到一个 WaitGroup 值的拷贝，因而当它们执行结束时，main 函数并不会知道
+		go waitGroupProcess(i, &wg)
+	}
+	// 一直阻塞
+	wg.Wait()
+
+	// select
+	{
+		output1 := make(chan  string)
+		output2 := make(chan  string)
+		go selectServer1(output1)
+		go selectServer2(output2)
+
+		// select 会一直发生阻塞，除非其中有 case 准备就绪
+		// 分析selectServer1 selectServer2 知道 一个休眠6秒 一个休眠3秒，因此 select 语句会阻塞 3 秒钟
+		// 因此整个select 语句块的输出是：from selectServer2
+		select {
+			case s1 := <- output1:
+				fmt.Println(s1)
+			case s2 := <- output2:
+				fmt.Println(s2)
+		}
+
+		// 注意select 的 default 选项:和switch 类型，如果都不匹配则进入default
+
+		ch := make(chan string)
+		// 会死锁：我们在 select 内部（第 6 行），试图读取信道 ch。由于没有 Go 协程向该信道写入数据
+		//select {
+		//	case <- ch:
+		//}
+
+		// 如果存在默认情况，就不会发生死锁，因为在没有其他 case 准备就绪时，会执行默认情况
+		select {
+			case <- ch:
+				{
+
+				}
+		default:
+			fmt.Println("default case execute")
+		}
+
+		// 如果 select 只含有值为 nil 的信道，也同样会执行默认情况
+		{
+			// ch 等于nil
+			// ch 等于 nil，而我们试图在 select 中读取 ch, 如果没有默认情况，select 会一直阻塞，导致死锁
+			var ch chan string
+			select {
+				case v := <-ch:
+					fmt.Println("received value", v)
+				default:
+					fmt.Println("default case executed")
+			}
+		}
+
+		// 随机选取
+		// 当 select 由多个 case 准备就绪时，将会随机地选取其中之一去执行
+		{
+			output1 := make(chan string)
+			output2 := make(chan string)
+
+			go selectRandomeServer1(output1)
+			go selectRandomeServer1(output2)
+
+			time.Sleep(time.Second * 1)
+
+			// 这个 select 语句中的两种情况都准备好执行了
+			// 如果你运行这个程序很多次的话，输出会是 from selectRandomeServer1 或者 from selectRandomeServer2，这会根据随机选取的结果而变化
+			select {
+				case s1 := <-output1:
+					fmt.Println(s1)
+				case s2 := <-output2:
+					fmt.Println(s2)
+			}
+		}
+
+		// 空select{}
+		// 除非有 case 执行，select 语句就会一直阻塞着。在这里，select 语句没有任何 case，因此它会一直阻塞，导致死锁
+		// fatal error: all goroutines are asleep - deadlock!
+		//select {
+		//
+		//}
 	}
 
 	fmt.Println("UseCase15Concurrency End")
